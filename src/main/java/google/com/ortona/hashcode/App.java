a19a061f10ba94009cfcbb56efcaf8e66a3741ab
Stefano Ortona <stefano@ip-10-76-8-99.eu-west-1.compute.internal>
2018-02-15 19:14:41 +0000

    add loggin and guava funcionality

---
+++ src/main/java/google/com/ortona/hashcode/App.java
@@ -0,0 +1,30 @@
+/*
+ * MIT License
+
+ * Copyright (c) 2016
+ */
+package google.com.ortona.hashcode;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Lists;
+
+/**
+ * Hello world!
+ *
+ * @author <a href="mailto:${developer-email}">${developer-name}</a>
+ * @version 1.0-SNAPSHOT
+ **/
+public class App {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+  public static void main(final String[] args) {
+    LOGGER.info("Hello world");
+    final List<String> list = Lists.newLinkedList();
+    LOGGER.info("This is an empty list '{}'", list);
+  }
+}
---
+++ src/test/java/google/com/ortona/hashcode/AppTest.java
@@ -0,0 +1,19 @@
+/*
+ * MIT License
+
+ * Copyright (c) 2016
+ */
+package google.com.ortona.hashcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test
+ */
+public class AppTest {
+  @Test
+  public void testScratch() {
+    Assert.assertTrue(true);
+  }
+}
