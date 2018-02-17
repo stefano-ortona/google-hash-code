b8b25b46e756a560968caf4e027aeb1b1ae5b313
giorgio <giopag27@gmail.com>
2018-02-17 19:40:49 +0100

    completed script

--- src/main/java/google/com/pagliaro/hashcode/pizza/script.rb
+++ src/main/java/google/com/pagliaro/hashcode/pizza/script.rb
@@ -14,15 +14,17 @@ def validate_output input_folder='files/example.in', output_folder='files/exampl
       pizza[[index, ingr_index]] = ingredient 
     }
   }
+  max = rows * cols
   
   output_lines = File.readlines(output_folder)
   slices_number = output_lines[0].to_i
   slices = output_lines[1..-1]
+  cells = 0
   slices.each{ |slice|
     r1, c1, r2, c2 = slice.split(' ').map(&:to_i) 
     # check number of slices
     squares_number = (r2 - r1 + 1) * (c2 - c1 + 1)
-    raise StandardError.new, "too squares for #{slice}" if squares_nubmer > max_cells
+    raise StandardError.new, "too squares for #{slice}" if squares_number > max_cells
     ingredients = []
     number_of_mushrooms = 0
     number_of_tomatoes = 0
@@ -33,11 +35,13 @@ def validate_output input_folder='files/example.in', output_folder='files/exampl
       (c1..c2).each{ |c|
         p c
          ingredients << pizza[[r,c]]
-         pizza[[r,c]] == 'T' ? number_of_mushrooms += 1 : number_of_tomatos += 1
+         pizza[[r,c]] == 'T' ? number_of_mushrooms += 1 : number_of_tomatoes += 1
          enough_tomatos = true if number_of_tomatoes >= min_per_ingredient 
          enough_mushrooms = true if number_of_mushrooms >= min_per_ingredient 
       } 
     }
     raise StandardError.new, "not_enough tomatos and mushs for #{slice} with #{ingredients}" unless enough_tomatos && enough_mushrooms 
+    cells += squares_number 
   }
+  p "output of #{cells} instead of max: #{max}"
 end
