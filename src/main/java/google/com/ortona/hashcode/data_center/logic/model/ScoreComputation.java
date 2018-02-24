0d71cfdbc699d029a6c228ffa2dce6c44d0ad388
Carlo Luchessa <c.luchessa@reply.it>
2018-02-24 18:24:08 +0100

    sticazzi

--- src/main/java/google/com/ortona/hashcode/data_center/logic/model/ScoreComputation.java
+++ src/main/java/google/com/ortona/hashcode/data_center/logic/model/ScoreComputation.java
@@ -1,10 +1,29 @@
 package google.com.ortona.hashcode.data_center.logic.model;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ScoreComputation {
 
-	public int computeScore(int pool, Datacenter center) {
+	public int computeScore(int poolId, Datacenter center) {
+		List<Server> poolServers = center.getPool(poolId);
+		Map<Integer, Integer> rowScores = new HashMap<>();
 		
-		return -1;
+		for (Server server : poolServers) {
+			int currentRow = server.getInitialSlot().row;
+			Integer currentRowScore = rowScores.get(currentRow);
+			if (currentRowScore == null) {
+				currentRowScore = 0;
+			}
+			currentRowScore += server.getCapacity();
+			rowScores.put(currentRow, currentRowScore);
+		}
+		
+		int maxScore = Collections.max(rowScores.values());
+
+		return maxScore;
 	}
 
 	public int computeScore(int pool, Datacenter center, Server toAdd, int row) {
