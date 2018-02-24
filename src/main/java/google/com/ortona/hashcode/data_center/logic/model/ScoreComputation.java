e0c3a0c803e7da7660cbc3954b86aaa86556953b
Carlo Luchessa <c.luchessa@reply.it>
2018-02-24 20:07:13 +0100

    sticazzi

--- src/main/java/google/com/ortona/hashcode/data_center/logic/model/ScoreComputation.java
+++ src/main/java/google/com/ortona/hashcode/data_center/logic/model/ScoreComputation.java
@@ -1,5 +1,6 @@
 package google.com.ortona.hashcode.data_center.logic.model;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
@@ -9,58 +10,67 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class ScoreComputation {
 
-  public int computeScore(int poolId, Datacenter center) {
-    final List<Server> poolServers = center.getPool(poolId);
+	public int computeScore(int poolId, Datacenter center) {
+		final List<Server> poolServers = center.getPool(poolId);
 
-    final Map<Integer, Integer> row2ScoreMap = calculateRow2ScoreMap(poolServers);
+		final Map<Integer, Integer> row2ScoreMap = calculateRow2ScoreMap(poolServers);
 
-    final Collection<Integer> scores = row2ScoreMap.values();
+		final Collection<Integer> scores = row2ScoreMap.values();
 
-    return calculateMaxScores(scores);
-  }
+		return calculateMaxScores(scores);
+	}
 
-  public int computeScore(int poolId, Datacenter center, Server serverToAdd, int row) {
-    final List<Server> poolServers = center.getPool(poolId);
+	public int computeScore(int poolId, Datacenter center, Server serverToAdd, int row) {
+		final List<Server> poolServers = center.getPool(poolId);
 
-    final Map<Integer, Integer> row2ScoreMap = calculateRow2ScoreMap(poolServers);
+		final Map<Integer, Integer> row2ScoreMap = calculateRow2ScoreMap(poolServers);
 
-    addCapacityToMap(row2ScoreMap, row, serverToAdd.getCapacity());
+		addCapacityToMap(row2ScoreMap, row, serverToAdd.getCapacity());
 
-    final Collection<Integer> scores = row2ScoreMap.values();
+		final Collection<Integer> scores = row2ScoreMap.values();
 
-    return calculateMaxScores(scores);
-  }
+		return calculateMaxScores(scores);
+	}
 
-  /*
-   * Internal methods
-   */
+	public int computeTotalScore(Datacenter center, int poolNum) {
+		List<Integer> poolScores = new ArrayList<>(poolNum);
+		for (int poolId = 0; poolId < poolNum; poolId++) {
+			poolScores.add(computeScore(poolId, center));
+		}
+		return Collections.min(poolScores);
+	}
 
-  private static Map<Integer, Integer> calculateRow2ScoreMap(List<Server> poolServers) {
-    final Map<Integer, Integer> row2ScoreMap = new HashMap<>();
 
-    for (final Server server : poolServers) {
-      final int currentRow = server.getInitialSlot().row;
-      addCapacityToMap(row2ScoreMap, currentRow, server.getCapacity());
-    }
+	/*
+	 * Internal methods
+	 */
 
-    return row2ScoreMap;
-  }
+	private static Map<Integer, Integer> calculateRow2ScoreMap(List<Server> poolServers) {
+		final Map<Integer, Integer> row2ScoreMap = new HashMap<>();
 
-  private static void addCapacityToMap(Map<Integer, Integer> row2ScoreMap, int currentRow, int capacity) {
-    Integer currentRowScore = row2ScoreMap.get(currentRow);
-    if (currentRowScore == null) {
-      currentRowScore = 0;
-    }
-    currentRowScore += capacity;
-    row2ScoreMap.put(currentRow, currentRowScore);
-  }
+		for (final Server server : poolServers) {
+			final int currentRow = server.getInitialSlot().row;
+			addCapacityToMap(row2ScoreMap, currentRow, server.getCapacity());
+		}
 
-  private static int calculateMaxScores(Collection<Integer> scores) {
-    final int maxScore = Collections.max(scores);
-    scores.remove(maxScore);
-    final AtomicInteger finalScore = new AtomicInteger(0);
-    scores.forEach(t -> finalScore.getAndAdd(t));
-    return finalScore.get(); // second max score
-  }
+		return row2ScoreMap;
+	}
+
+	private static void addCapacityToMap(Map<Integer, Integer> row2ScoreMap, int currentRow, int capacity) {
+		Integer currentRowScore = row2ScoreMap.get(currentRow);
+		if (currentRowScore == null) {
+			currentRowScore = 0;
+		}
+		currentRowScore += capacity;
+		row2ScoreMap.put(currentRow, currentRowScore);
+	}
+
+	private static int calculateMaxScores(Collection<Integer> scores) {
+		final int maxScore = Collections.max(scores);
+		scores.remove(maxScore);
+		final AtomicInteger finalScore = new AtomicInteger(0);
+		scores.forEach(t -> finalScore.getAndAdd(t));
+		return finalScore.get(); // second max score
+	}
 
 }
