2ea1446c68c8b2836d02b9f67c246e8b80d11984
Stefano Ortona <stefano@ip-10-76-9-134.eu-west-1.compute.internal>
2018-02-24 14:39:42 +0000

    add entry points for optimize data center qualification round

---
+++ src/main/java/google/com/ortona/hashcode/data_center/logic/OptimizeDataCenterLogic.java
@@ -0,0 +1,5 @@
+package google.com.ortona.hashcode.data_center.logic;
+
+public class OptimizeDataCenterLogic {
+
+}
---
+++ src/test/java/google/com/ortona/hashcode/data_center/OptimizeDataCenterLogicTest.java
@@ -0,0 +1,5 @@
+package google.com.ortona.hashcode.data_center;
+
+public class OptimizeDataCenterLogicTest {
+
+}
