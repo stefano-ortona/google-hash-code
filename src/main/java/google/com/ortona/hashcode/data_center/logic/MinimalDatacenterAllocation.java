a8474bcf8908fcb2955cb1cb07cbb44c4b014b98
Carlo Luchessa <c.luchessa@reply.it>
2018-02-24 18:50:15 +0100

    sticazzi

--- src/main/java/google/com/ortona/hashcode/data_center/logic/MinimalDatacenterAllocation.java
+++ src/main/java/google/com/ortona/hashcode/data_center/logic/MinimalDatacenterAllocation.java
@@ -14,32 +14,31 @@ import org.slf4j.LoggerFactory;
 
 public class MinimalDatacenterAllocation {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
-  public static void allocateServer(List<Server> servers, int numPool, Datacenter center) {
-    Collections.sort(servers, new ServerComparator());
+	public static void allocateServer(List<Server> servers, int numPool, Datacenter center) {
+		Collections.sort(servers, new ServerComparator());
 
-    for (int currentPool= 0; currentPool< numPool; currentPool++) {
-    		Slot slot =  null;
-    		for (int j=0; j< 2; j++) { // 2 server
-    			Server server = popServer(servers);
-    			if (slot == null) {
-    				slot = center.getNextAvailableSlot(server);
-    			} else {
-    				slot = center.getNextAvailableSlot(server, slot.getRow());
-    			}
-    			if (slot == null) {
-    				LOGGER.info("ERROR: NO SLOT RETRIEVED");
-    				continue; // ERR
-    			}
-    			center.addServer(server, slot, currentPool);
-    		}
-    }
-    
-  }
-  
-  private static Server popServer(List<Server> servers) {
-	  return servers.remove(0);
-  }
+		for (int currentPool= 0; currentPool< numPool; currentPool++) {
+			Slot slot =  null;
+			for (int j=0; j< 2; j++) { // 2 server
+				Server server = popServer(servers);
+				if (slot == null) {
+					slot = center.getNextAvailableSlot(server);
+				} else {
+					slot = center.getNextAvailableSlot(server, slot.getRow());
+				}
+				if (slot == null) {
+					LOGGER.info("ERROR: NO SLOT RETRIEVED");
+					continue; // ERR
+				}
+				center.addServer(server, slot, currentPool);
+			}
+		}
+	}
+
+	private static Server popServer(List<Server> servers) {
+		return servers.remove(0);
+	}
 
 }
