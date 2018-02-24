f2fa7076bd8a70ead1a808ea25da0183a3c84170
Carlo Luchessa <c.luchessa@reply.it>
2018-02-24 18:12:04 +0100

    sticazzi

--- src/test/java/google/com/ortona/hashcode/data_center/OptimizeDataCenterLogicTest.java
+++ src/test/java/google/com/ortona/hashcode/data_center/OptimizeDataCenterLogicTest.java
@@ -1,5 +1,31 @@
 package google.com.ortona.hashcode.data_center;
 
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import google.com.ortona.hashcode.data_center.logic.MinimalDatacenterAllocation;
+import google.com.ortona.hashcode.data_center.logic.model.Datacenter;
+import google.com.ortona.hashcode.data_center.logic.model.Server;
+
 public class OptimizeDataCenterLogicTest {
 
+	int numPool;
+	Datacenter datacenter;
+	List<Server> servers;
+	
+	@BeforeClass
+	public static void bringUp() {
+		
+
+	}
+
+	@Test
+	public void testToyExample() {
+		MinimalDatacenterAllocation.allocateServer(servers, numPool, datacenter);
+		
+		// todo
+	}
+	
 }
