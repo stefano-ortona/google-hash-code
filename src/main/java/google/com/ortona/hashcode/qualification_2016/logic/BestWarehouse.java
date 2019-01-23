f5b223a2df22047f5b8901b147b430e273f6356d
Carlo Luchessa <c.luchessa@reply.it>
2019-01-23 23:08:24 +0100

    up.

--- src/main/java/google/com/ortona/hashcode/qualification_2016/logic/BestWarehouse.java
+++ src/main/java/google/com/ortona/hashcode/qualification_2016/logic/BestWarehouse.java
@@ -1,16 +1,44 @@
 package google.com.ortona.hashcode.qualification_2016.logic;
 
+import java.util.HashMap;
 import java.util.Map;
 
-import google.com.ortona.hashcode.qualification_2016.model.Drone;
-import google.com.ortona.hashcode.qualification_2016.model.ProblemContainer;
-import google.com.ortona.hashcode.qualification_2016.model.Product;
-import google.com.ortona.hashcode.qualification_2016.model.Warehouse;
+import google.com.ortona.hashcode.qualification_2016.model.*;
 
 public class BestWarehouse {
 
-  public Map<Warehouse, Product> getBestWarehouse(Drone d, ProblemContainer problem) {
+    public BestWarehouseResult getBestWarehouse(Drone drone, ProblemContainer problem) {
+        BestWarehouseResult result = new BestWarehouseResult();
 
-  }
+        int bestPathDistance = Integer.MAX_VALUE;
+
+        for (Order order : problem.getOrders()) {
+            // per ogni prodotto di ogni ordine calcolo la distanza dalla warehouse e prendo la migliore
+
+            for (Map.Entry<Product, Integer> productIntegerEntry : order.getProducts2quantity().entrySet()) {
+                for (Warehouse warehouse : problem.getWarehouses()) {
+                    int pathDistance = calculatePathDistance(drone, warehouse, order);
+                    if (pathDistance < bestPathDistance) {
+                        bestPathDistance = pathDistance;
+                        // TODO: 2019-01-23
+                        result.setOrder(order);
+                        result.setProduct(productIntegerEntry.getKey());
+                        result.setWarehouse(warehouse);
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+
+    /*
+     * Internal methods
+     */
+
+    private int calculatePathDistance(Drone drone, Warehouse warehouse, Order order) {
+        return 0;
+    }
 
 }
