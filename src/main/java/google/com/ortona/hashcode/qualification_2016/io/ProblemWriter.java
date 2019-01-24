b829a002088114ce85b8623de6c0f66924860bd9
giorgio <giopag27@gmail.com>
2019-01-24 17:44:51 +0100

    problemWriter first version

--- src/main/java/google/com/ortona/hashcode/qualification_2016/io/ProblemWriter.java
+++ src/main/java/google/com/ortona/hashcode/qualification_2016/io/ProblemWriter.java
@@ -1,11 +1,23 @@
 package google.com.ortona.hashcode.qualification_2016.io;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import google.com.ortona.hashcode.qualification_2016.model.Action;
 import google.com.ortona.hashcode.qualification_2016.model.SolutionContainer;
 
 public class ProblemWriter {
-
-  public void writeProblem(String outputFile, SolutionContainer solution) {
-    // TODO
-  }
-
+	public static void writeProblem(String outputFile, SolutionContainer solution)
+	    throws FileNotFoundException, UnsupportedEncodingException {
+		final PrintWriter writer = new PrintWriter(outputFile, "UTF-8");
+		for (final Action action : solution.getActions()) {
+			writer.print(action.getDrone().getId());
+			writer.print(action.getType());
+			writer.print(action.getId());
+			writer.print(action.getProductId());
+			writer.print(action.getQuantity());
+			writer.println();
+		}
+		writer.close();
+	}
 }
