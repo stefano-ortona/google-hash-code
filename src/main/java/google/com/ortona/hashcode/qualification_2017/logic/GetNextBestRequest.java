01b487931c0b45ee0f8ca01ceac48fba49f585eb
Stefano Ortona <stefano.ortona@meltwater.com>
2019-02-06 20:03:42 +0000

    Best request selector class

---
+++ src/main/java/google/com/ortona/hashcode/qualification_2017/logic/GetNextBestRequest.java
@@ -0,0 +1,16 @@
+package google.com.ortona.hashcode.qualification_2017.logic;
+
+import java.util.List;
+import java.util.Map;
+
+import google.com.ortona.hashcode.qualification_2017.model.Cache;
+import google.com.ortona.hashcode.qualification_2017.model.Request;
+
+public class GetNextBestRequest {
+
+	public Map<Request, Cache> getNextRequest(List<Request> requests) {
+		// TODO
+		return null;
+	}
+
+}
