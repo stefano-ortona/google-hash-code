3f7da811fc1ade873cc506808a82c01ec04fe1c9
Stefano Ortona <stefano.ortona@meltwater.com>
2019-02-06 20:04:36 +0000

    Equals hashcode for request

--- src/main/java/google/com/ortona/hashcode/qualification_2017/model/Request.java
+++ src/main/java/google/com/ortona/hashcode/qualification_2017/model/Request.java
@@ -38,4 +38,30 @@ public class Request {
 		this.e = e;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = (prime * result) + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final Request other = (Request) obj;
+		if (id != other.id) {
+			return false;
+		}
+		return true;
+	}
+
 }
