58009f30a44363b2d90b168ced472051f73cf804
Carlo Luchessa <c.luchessa@reply.it>
2019-02-06 22:25:04 +0100

    up.

--- src/main/java/google/com/ortona/hashcode/qualification_2017/logic/GetNextBestRequest.java
+++ src/main/java/google/com/ortona/hashcode/qualification_2017/logic/GetNextBestRequest.java
@@ -3,9 +3,10 @@ package google.com.ortona.hashcode.qualification_2017.logic;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Consumer;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import google.com.ortona.hashcode.qualification_2017.model.Cache;
+import google.com.ortona.hashcode.qualification_2017.model.Pair;
 import google.com.ortona.hashcode.qualification_2017.model.Request;
 import google.com.ortona.hashcode.qualification_2017.model.Video;
 
@@ -15,19 +16,37 @@ public class GetNextBestRequest {
 
         Map<Request, Cache> result = new HashMap<>();
 
-        int scoreBest = 0;
+        AtomicInteger scoreBest = new AtomicInteger(0);
 
-        requests.forEach(request -> request.getE().getCache2latency().entrySet()
-                .stream()
-                .filter(cacheIntegerEntry -> isCacheLargeEnough(request, cacheIntegerEntry))
-                .forEach(cacheIntegerEntry -> {
-                    Cache cache = cacheIntegerEntry.getKey();
-                    int score = calculateScore(request.getQuantity(), request.getE().getDataCenterLatency(), cacheIntegerEntry.getValue());
-                    if (score > scoreBest) {
-                        updateResult(result, request, cache);
+        requests.forEach(request -> {
+
+            Pair<Cache, Integer> currentResult = new Pair<>();
+
+            request.getE().getCache2latency().entrySet().stream()
+                    .filter(cacheIntegerEntry -> isCacheLargeEnough(request, cacheIntegerEntry))
+                    .forEach(cacheIntegerEntry -> {
+                        Cache cache = cacheIntegerEntry.getKey();
+                        int score = calculateScore(request.getQuantity(), request.getE().getDataCenterLatency(), cacheIntegerEntry.getValue());
+                        if (currentResult.getValue() == null || score > currentResult.getValue()) {
+                            currentResult.setKey(cache);
+                            currentResult.setValue(score);
+                        }
+                    });
+
+            if (currentResult.getKey() != null && !currentResult.getKey().getVideos().contains(request.getV())) {
+
+                if (!isAlreadyInOtherEfficientCache(request.getV(), currentResult.getKey(), request.getE().getCache2latency())) {
+
+                    if (currentResult.getValue() != null && currentResult.getValue() > scoreBest.get()) {
+                        updateResult(result, request, currentResult.getKey());
+                        scoreBest.set(currentResult.getValue());
                     }
-                }));
-        return result.isEmpty() ? null : result;
+                }
+            }
+
+        });
+
+        return result.isEmpty() ? null : result; // TODO: 2019-02-06 attenzione a oggetto fake
     }
 
 
@@ -41,6 +60,23 @@ public class GetNextBestRequest {
 
     private int calculateScore(int quantityRequest, int endpointLatency, int cacheLatency) {
         return quantityRequest * (endpointLatency - cacheLatency);
+    }
+
+
+    private boolean isAlreadyInOtherEfficientCache(Video video, Cache cacheBest, Map<Cache, Integer> cache2latency) {
+        return false;
+        /*
+        Optional<Map.Entry<Cache, Integer>> otherCache2LatencyWithVideo = cache2latency.entrySet()
+                .stream()
+                .filter(new Predicate<Map.Entry<Cache, Integer>>() {
+                    @Override
+                    public boolean test(Map.Entry<Cache, Integer> cacheIntegerEntry) {
+                        return cacheIntegerEntry.getKey().getVideos().contains(video)
+                                && !cacheIntegerEntry.getKey().equals(cacheBest);
+                    }
+                })
+                .findFirst();
+                */
     }
 
     private void updateResult(Map<Request, Cache> result, Request request, Cache cache) {
---
+++ src/main/java/google/com/ortona/hashcode/qualification_2017/model/Pair.java
@@ -0,0 +1,32 @@
+package google.com.ortona.hashcode.qualification_2017.model;
+
+public class Pair<K, V> {
+
+    K key;
+
+    V value;
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "key=" + key +
+                ", value=" + value +
+                '}';
+    }
+}
