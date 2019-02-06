4155812d7e5940bd66801c7e5648c4dd000e652e
Stefano Ortona <stefano.ortona@meltwater.com>
2019-02-06 18:13:31 +0000

    Add skeleton project for 2017 qualification round

---
+++ src/main/java/google/com/ortona/hashcode/qualification_2017/logic/ProblemSolver.java
@@ -0,0 +1,18 @@
+package google.com.ortona.hashcode.qualification_2017.logic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import google.com.ortona.hashcode.qualification_2017.model.ProblemContainer;
+import google.com.ortona.hashcode.qualification_2017.model.SolutionContainer;
+
+public class ProblemSolver {
+
+  Logger LOG = LoggerFactory.getLogger(getClass());
+
+  public SolutionContainer process(ProblemContainer problem) {
+    // TODO
+    return null;
+  }
+
+}
