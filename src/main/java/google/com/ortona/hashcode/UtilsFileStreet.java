64fc4850509614c9e9a37e8ffd8da9d6eee8c439
Daniele Salvatore <danieleslvtr@gmail.com>
2019-02-20 21:53:21 +0100

    Reader

---
+++ src/main/java/google/com/ortona/hashcode/UtilsFileStreet.java
@@ -0,0 +1,255 @@
+package google.com.ortona.hashcode;
+
+import google.com.ortona.hashcode.final_2014.model.Car;
+import google.com.ortona.hashcode.final_2014.model.Junction;
+import google.com.ortona.hashcode.final_2014.model.Street;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class UtilsFileStreet {
+
+    /* If file structure is preserved (i.e. first line header, rest of the file data)
+        Edit this file as following:
+        1. define type of header items and data items
+        2. generate setters and getters for header and data
+        3. define logic of createHeader() and createData()
+     */
+
+    // 1. define type of header items and data items
+    private int[] header;
+    private List<Car> cars;
+    private Map<Integer, Car> id2car;
+    private List<Street> streets;
+    private Map<Integer, Street> id2street;
+    private List<Junction> junctions;
+    private Map<Integer, Junction> id2junction;
+
+    private char[][] data;
+
+    // 2. generate setters and getters for header and data
+    public void setHeader(int[] header) {
+        this.header = header;
+    }
+
+    public void setData(char[][] data) {
+        this.data = data;
+    }
+
+    public int[] getHeader() {
+        return header;
+    }
+
+    public char[][] getData() {
+        return data;
+    }
+
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public Map<Integer, Car> getId2car() {
+        return id2car;
+    }
+
+    public void setId2car(Map<Integer, Car> id2car) {
+        this.id2car = id2car;
+    }
+
+    public List<Street> getStreets() {
+        return streets;
+    }
+
+    public void setStreets(List<Street> streets) {
+        this.streets = streets;
+    }
+
+    public Map<Integer, Street> getId2street() {
+        return id2street;
+    }
+
+    public void setId2street(Map<Integer, Street> id2street) {
+        this.id2street = id2street;
+    }
+
+    public List<Junction> getJunctions() {
+        return junctions;
+    }
+
+    public void setJunctions(List<Junction> junctions) {
+        this.junctions = junctions;
+    }
+
+    public Map<Integer, Junction> getId2junction() {
+        return id2junction;
+    }
+
+    public void setId2junction(Map<Integer, Junction> id2junction) {
+        this.id2junction = id2junction;
+    }
+
+    //3. define logic of createHeader() and createData()
+
+    public void createHeader() {
+        String firstLine = getFirstLineOfFile();
+        String[] split = splitString(firstLine, " ");
+        int[] converted = convertArrayOfStringToArrayOfInt(split);
+
+        // always finish with this.setHeader()
+        this.setHeader(converted);
+    }
+
+    //○ N denotes the number of junctions in the city
+    public int getJunctionAmount() {
+        return this.getHeader()[0];
+    }
+
+    //○ M denotes the number of streets in the city
+    public int getStreetAmount() {
+        return this.getHeader()[1];
+    }
+
+    //○ T denotes the virtual time in seconds allowed for the car itineraries
+    public int getAllowedTime() {
+        return this.getHeader()[2];
+    }
+
+    //○ C denotes the number of cars in the fleet
+    public int getCarAmount() {
+        return this.getHeader()[3];
+    }
+
+    //○ S denotes the junction at which all the cars are located initially
+    public int getInitialJunction() {
+        return this.getHeader()[4];
+    }
+
+    // Car
+    public void createCars() {
+        cars = new ArrayList<>();
+        id2car = new HashMap<>();
+
+        for (int i = 0; i < this.getCarAmount(); i++) {
+            Car c = new Car();
+            c.setId(i);
+            cars.add(c);
+            id2car.put(i, c);
+        }
+
+        this.setCars(cars);
+        this.setId2car(id2car);
+    }
+
+    // Junction
+//    public void createJunctions() {
+//        String[] file = this.getFile();
+//        String[] dataRaw = cloneArrayOfString(file, 1, this.getJunctionAmount());
+//
+//        // always finish with this.setData()
+//        this.setData(matrix);
+//    }
+
+
+    // ====== Do not change below here
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+    private static final String RESOURCE_PATH = "src/main/resources/google/com/ortona/hashcode/final_2014/";
+    private String[] file;
+
+    public void setFile(String[] file) {
+        this.file = file;
+    }
+
+    public String[] getFile() {
+        return file;
+    }
+
+    // Constructor
+    public UtilsFileStreet(String filepath) {
+
+        try {
+            File file = new File(RESOURCE_PATH + filepath);
+            String absolutePath = file.getAbsolutePath();
+
+            LOGGER.info("File absolute path:" + absolutePath);
+            readFile(absolutePath);
+
+            //LOGGER.info("Header creation: start");
+            createHeader();
+            //LOGGER.info("Header creation: done");
+
+            //LOGGER.info("Header creation: start");
+            createCars();
+            //LOGGER.info("Header creation: done");
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    // Utils
+    public void readFile(String filepath) throws IOException {
+
+        String line;
+        List<String> list = new ArrayList<>();
+        BufferedReader in = new BufferedReader(new FileReader(filepath));
+
+        while ((line = in.readLine()) != null) {
+            list.add(line);  //Add line to file
+        }
+
+        //Cast List to Array of Array
+        String[] stringArr = list.toArray(new String[0]);
+
+        this.setFile(stringArr);
+
+    }
+
+    public String getFirstLineOfFile() {
+        return this.file[0];
+    }
+
+    public String[] splitString(String string, String separator) {
+        return string.split(separator);
+    }
+
+    public String[] cloneArrayOfString(String[] source, Integer start, Integer to) {
+        return Arrays.copyOfRange(source, start, to);
+    }
+
+    public char[] convertStringToArrayOfChar(String string) {
+        return string.toCharArray();
+    }
+
+    public char[][] convertArrayOfStringToArrayOfCharArrays(String[] dataRaw) {
+
+        char[][] result = new char[dataRaw.length][dataRaw[0].length()];
+
+        for (int i = 0; i < dataRaw.length; i++) {
+            result[i] = convertStringToArrayOfChar(dataRaw[i]);
+        }
+        return result;
+    }
+
+    public String convertArrayOfChartToString(char[] a) {
+        return new String(a);
+    }
+
+    public int[] convertArrayOfStringToArrayOfInt(String[] strings) {
+        return Arrays.asList(strings).stream().mapToInt(Integer::parseInt).toArray();
+
+    }
+
+}
---
+++ src/test/java/google/com/ortona/hashcode/final_2014/UtilsFileStreetTest.java
@@ -0,0 +1,82 @@
+/*
+ * MIT License
+
+ * Copyright (c) 2016
+ */
+package google.com.ortona.hashcode.final_2014;
+
+import google.com.ortona.hashcode.UtilsFileStreet;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class UtilsFileStreetTest {
+
+    private final String filePathExample = "example.in";
+    private UtilsFileStreet fr = new UtilsFileStreet(filePathExample);
+    // Header
+    private int[] headerExample = fr.getHeader();
+    private int[] actualHeaderExample = new int[]{3, 2, 3000, 2, 0};
+    // Cars
+
+
+    // Problem Container
+    //private ProblemContainer problemContainer = fr.getProblemContainer();
+
+
+    // Example.in file
+
+    // Header
+    @Test
+    public void testHeaderExample() {
+
+        testHeader(actualHeaderExample, headerExample);
+        Assert.assertEquals(fr.getJunctionAmount(), actualHeaderExample[0]);
+        Assert.assertEquals(fr.getStreetAmount(), actualHeaderExample[1]);
+        Assert.assertEquals(fr.getAllowedTime(), actualHeaderExample[2]);
+        Assert.assertEquals(fr.getCarAmount(), actualHeaderExample[3]);
+        Assert.assertEquals(fr.getInitialJunction(), actualHeaderExample[4]);
+    }
+
+    @Test
+    public void testCars(){
+        Assert.assertEquals(fr.getCarAmount(), fr.getCars().size());
+        for (int i = 0; i < fr.getCars().size(); i++) {
+            Assert.assertEquals(i, fr.getCars().get(i).getId());
+        }
+    }
+
+    // ============================ utils
+    private void testHeader(int[] actualHeader, int[] header) {
+
+        for (int i = 0; i < actualHeader.length; i++) {
+            Assert.assertEquals(header[i], actualHeader[i]);
+        }
+    }
+
+    private void testDataLineByLine(String[] actualData, char[][] data) {
+
+        // line by line
+        for (int i = 0; i < actualData.length; i++) {
+            String d = fr.convertArrayOfChartToString(data[i]);
+            Assert.assertEquals(d, actualData[i]);
+        }
+
+    }
+
+    private void testDataCharByChar(String[] actualData, char[][] data) {
+
+        for (int i = 0; i < actualData.length; i++) {
+            testCharByChartLine(actualData[i], data[i]);
+        }
+
+    }
+
+    private void testCharByChartLine(String line, char[] data) {
+
+        for (int i = 0; i < line.length(); i++) {
+            Assert.assertEquals(line.charAt(i), data[i]);
+        }
+
+    }
+}
