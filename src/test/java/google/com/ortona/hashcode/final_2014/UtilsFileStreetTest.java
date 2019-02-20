403a3b306ca0bf77bb889641532e29807ec111e0
Daniele Salvatore <danieleslvtr@gmail.com>
2019-02-20 22:50:41 +0100

    Reader

--- src/test/java/google/com/ortona/hashcode/final_2014/UtilsFileStreetTest.java
+++ src/test/java/google/com/ortona/hashcode/final_2014/UtilsFileStreetTest.java
@@ -50,11 +50,11 @@ public class UtilsFileStreetTest {
     }
 
     @Test
-    public void testCars(){
+    public void testCars() {
         Assert.assertEquals(fr.getCarAmount(), fr.getCars().size());
         for (int i = 0; i < fr.getCars().size(); i++) {
             Car c = fr.getCars().get(i);
-            Junction  j =new Junction();
+            Junction j = new Junction();
             j.setId(fr.getInitialJunction());
             Assert.assertEquals(i, c.getId());
             Assert.assertEquals(c.getCurrent(), j);
@@ -62,23 +62,23 @@ public class UtilsFileStreetTest {
     }
 
     @Test
-    public void testJunctions(){
+    public void testJunctions() {
 
         junctionsActual = new ArrayList<>();
 
-        Junction j0  = new Junction();
+        Junction j0 = new Junction();
         j0.setId(0);
         j0.setLat(48.8582);
         j0.setLng(2.2945);
         junctionsActual.add(j0);
 
-        Junction j1  = new Junction();
+        Junction j1 = new Junction();
         j1.setId(1);
         j1.setLat(50.0);
         j1.setLng(3.09);
         junctionsActual.add(j1);
 
-        Junction j2  = new Junction();
+        Junction j2 = new Junction();
         j2.setId(2);
         j2.setLat(51.424242);
         j2.setLng(3.02);
@@ -96,26 +96,36 @@ public class UtilsFileStreetTest {
     }
 
     @Test
-    public void testStreets(){
+    public void testStreets() {
         streetsActual = new ArrayList<>();
 
-//        private Junction start;
-//        private Junction end;
-//        private double length; // contribute to the score
-//        private double timeCost;
-//        private boolean isBidirectional;
-//        private boolean isVisited;
-//
-//        Street s0 = new Street();
-//        s0.setStart(new Junction(fr.getId2junction()));
-//        s0.setStart(new Junction());
-//
-//
-//        01130250 //Streetfromfirstjunctiontosecondjunction.
-//        12245200 //Streetfromsecondjunctiontothirdjunction.
+        //0 1 1 30 250
+        Street s0 = new Street();
+        s0.setStart(fr.getId2junction().get(0));
+        s0.setEnd(fr.getId2junction().get(1));
+        s0.setBidirectional(false);
+        s0.setTimeCost(30);
+        s0.setLength(250);
+        streetsActual.add(s0);
+
+        //1 2 2 45 200
+        Street s1 = new Street();
+        s1.setStart(fr.getId2junction().get(1));
+        s1.setEnd(fr.getId2junction().get(2));
+        s1.setBidirectional(true);
+        s1.setTimeCost(45);
+        s1.setLength(200);
+        streetsActual.add(s1);
+
         Assert.assertEquals(fr.getStreetAmount(), fr.getStreets().size());
         for (int i = 0; i < fr.getStreets().size(); i++) {
-            //Assert.assertEquals(i, fr.getStreets().get(i));
+            Street actual = streetsActual.get(i);
+            Street street = streetsExample.get(i);
+            Assert.assertEquals(actual.getStart(), street.getStart());
+            Assert.assertEquals(actual.getEnd(), street.getEnd());
+            Assert.assertEquals(actual.isBidirectional(), street.isBidirectional());
+            Assert.assertEquals(actual.getTimeCost(), street.getTimeCost(), 0.001);
+            Assert.assertEquals(actual.getLength(), street.getLength(), 0.001);
         }
     }
 
