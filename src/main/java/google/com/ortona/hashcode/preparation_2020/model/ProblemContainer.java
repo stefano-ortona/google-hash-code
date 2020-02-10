4f197f9223a20a7e5ecf989232ce01713f6079df
Stefano Ortona <stefano.ortona@meltwater.com>
2020-02-10 20:10:00 +0000

    Add skeleton classes

--- src/main/java/google/com/ortona/hashcode/preparation_2020/model/ProblemContainer.java
+++ src/main/java/google/com/ortona/hashcode/preparation_2020/model/ProblemContainer.java
@@ -1,5 +1,32 @@
 package google.com.ortona.hashcode.preparation_2020.model;
 
+import java.util.List;
+
 public class ProblemContainer {
+
+  private List<Integer> numbers;
+
+  public List<Integer> getNumbers() {
+    return numbers;
+  }
+
+  public void setNumbers(List<Integer> numbers) {
+    this.numbers = numbers;
+  }
+
+  public int getGoal() {
+    return goal;
+  }
+
+  public void setGoal(int goal) {
+    this.goal = goal;
+  }
+
+  private int goal;
+
+  public ProblemContainer(List<Integer> numbers, int goal) {
+    this.numbers = numbers;
+    this.goal = goal;
+  }
 
 }
--- src/main/java/google/com/ortona/hashcode/preparation_2020/model/SolutionContainer.java
+++ src/main/java/google/com/ortona/hashcode/preparation_2020/model/SolutionContainer.java
@@ -1,10 +1,36 @@
 package google.com.ortona.hashcode.preparation_2020.model;
 
+import java.util.List;
+
 public class SolutionContainer {
 
-    @Override
-    public String toString() {
-        //TODO
-        return null;
-    }
+  private List<Integer> indexes;
+  private int score;
+
+  public List<Integer> getIndexes() {
+    return indexes;
+  }
+
+  public void setIndexes(List<Integer> indexes) {
+    this.indexes = indexes;
+  }
+
+  public int getScore() {
+    return score;
+  }
+
+  public void setScore(int score) {
+    this.score = score;
+  }
+
+  public SolutionContainer(List<Integer> indexes, int score) {
+    this.indexes = indexes;
+    this.score = score;
+  }
+
+  @Override
+  public String toString() {
+    // TODO
+    return null;
+  }
 }
