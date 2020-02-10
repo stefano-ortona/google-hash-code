f8bc2bea697e5d35ff67f0d1031c63142664d359
carlo luchessa <c.luchessa@reply.it>
2020-02-10 21:50:30 +0100

    up.

--- src/main/java/google/com/ortona/hashcode/preparation_2020/logic/ProblemSolver.java
+++ src/main/java/google/com/ortona/hashcode/preparation_2020/logic/ProblemSolver.java
@@ -6,23 +6,61 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import google.com.ortona.hashcode.preparation_2020.model.Pair;
 import google.com.ortona.hashcode.preparation_2020.model.ProblemContainer;
 import google.com.ortona.hashcode.preparation_2020.model.SolutionContainer;
 
 public class ProblemSolver {
-	private static Logger LOG = LoggerFactory.getLogger(ProblemSolver.class);
-	
-	public SolutionContainer solve(ProblemContainer problem) {
-	  List<Integer> allnumbers = problem.getNumbers();
-	  List<Integer> allResults = new ArrayList<>();
-	  //TODO
-	  
-	  SolutionContainer solution = new SolutionContainer(allResults, 0);
-	  return null;
-	}
-	
-	public static void main(String []args) {
-	  LOG.info("Hello World!");
-	}
+    private static Logger LOG = LoggerFactory.getLogger(ProblemSolver.class);
+
+    private static int CHUNK_LENGTH = 25;
+
+    private SubchankOptimal subChunkProcessor = new SubchankOptimal();
+
+    public SolutionContainer solve(ProblemContainer problem) {
+        List<Integer> allNumbers = problem.getNumbers();
+
+        int startIndex = 0;
+        int endIndex = allNumbers.size() - 1;
+
+        List<Integer> allResults = new ArrayList<>();
+
+        boolean isLeft = false;
+
+        while (startIndex < endIndex) {
+        	int chunkCurrentSize = 0;
+
+			List<Pair> chunk = new ArrayList<>();
+
+			while (chunkCurrentSize < CHUNK_LENGTH) {
+				if (startIndex >= endIndex) {
+					break;
+				}
+				if (isLeft) {
+					chunk.add(new Pair(startIndex, allNumbers.get(startIndex)));
+					startIndex++;
+				} else {
+					chunk.add(new Pair(endIndex, allNumbers.get(endIndex)));
+					endIndex--;
+				}
+				isLeft = !isLeft;
+			}
+
+			allResults.addAll(subChunkProcessor.solve(chunk, problem.getGoal()));
+        }
+
+        int totalScore = 0;
+		for (Integer index : allResults) {
+			totalScore += allNumbers.get(index);
+		}
+
+        SolutionContainer solution = new SolutionContainer(allResults, totalScore);
+
+        return solution;
+    }
+
+    public static void main(String[] args) {
+        LOG.info("Hello World!");
+    }
 
 }
