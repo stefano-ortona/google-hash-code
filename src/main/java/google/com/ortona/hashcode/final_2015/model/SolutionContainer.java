8d3501ebaeb515a357cfe9cae3e77adbe4bcc81b
carlo luchessa <c.luchessa@reply.it>
2020-02-18 22:41:57 +0100

    fix test dependencies.

--- src/main/java/google/com/ortona/hashcode/final_2015/model/SolutionContainer.java
+++ src/main/java/google/com/ortona/hashcode/final_2015/model/SolutionContainer.java
@@ -2,42 +2,37 @@ package google.com.ortona.hashcode.final_2015.model;
 
 import java.util.List;
 
-import org.junit.Assert;
-
-import google.com.ortona.hashcode.final_2014.model.Car;
-import google.com.ortona.hashcode.final_2014.model.Junction;
-
 public class SolutionContainer {
 
-	List<Baloon> baloons;
+    List<Baloon> baloons;
 
-	public SolutionContainer(List<Baloon> baloons) {
-		this.baloons = baloons;
-	}
+    public SolutionContainer(List<Baloon> baloons) {
+        this.baloons = baloons;
+    }
 
-	private int score;
+    private int score;
 
-	public void setScore(int score) {
-		this.score = score;
-	}
+    public void setScore(int score) {
+        this.score = score;
+    }
 
-	public int getScore() {
-		return this.score;
-	}
-	
-	public List<Baloon> getBaloons() {
-		return this.baloons;
-	}
+    public int getScore() {
+        return this.score;
+    }
 
-	@Override
-	public String toString() {
-		final StringBuilder string = new StringBuilder();
-	    for (int i=0; i<getBaloons().get(0).getMoves().size(); i++) {
-	    	for (final Baloon baloon : getBaloons()) {
-		    	string.append(baloon.getMoves().get(i));
-		    }
-	    	string.append("\n");
-	    }
-	    return string.toString();
-	}
+    public List<Baloon> getBaloons() {
+        return this.baloons;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder string = new StringBuilder();
+        for (int i = 0; i < getBaloons().get(0).getMoves().size(); i++) {
+            for (final Baloon baloon : getBaloons()) {
+                string.append(baloon.getMoves().get(i));
+            }
+            string.append("\n");
+        }
+        return string.toString();
+    }
 }
