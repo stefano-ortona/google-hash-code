7647b6908a127cef788b924544f89db3b32012cc
Daniele Salvatore <danieleslvtr@gmail.com>
2020-02-18 23:18:58 +0100

    reader

--- src/test/java/google/com/ortona/hashcode/UtilsBalloonsTest.java
+++ src/test/java/google/com/ortona/hashcode/UtilsBalloonsTest.java
@@ -1,12 +1,11 @@
 package google.com.ortona.hashcode;
 
-import google.com.ortona.hashcode.final_2015.model.ProblemContainer;
 import org.junit.Assert;
 import org.junit.Test;
-import java.util.ArrayList;
 
 public class UtilsBalloonsTest {
 
+    //static private final String filePathExample = "hashcode_2015_final_round.in";
     static private final String filePathExample = "test.in";
     static private UtilsBalloons fr = new UtilsBalloons(filePathExample);
 
