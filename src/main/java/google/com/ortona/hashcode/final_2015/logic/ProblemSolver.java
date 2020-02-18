1c022595c53f3084ce7a556753bae92c1c9aec8e
Stefano Ortona <stefano.ortona@meltwater.com>
2020-02-18 20:47:24 +0000

    Add problem solver implementation

--- src/main/java/google/com/ortona/hashcode/final_2015/logic/ProblemSolver.java
+++ src/main/java/google/com/ortona/hashcode/final_2015/logic/ProblemSolver.java
@@ -5,16 +5,29 @@ import org.slf4j.LoggerFactory;
 
 import google.com.ortona.hashcode.final_2015.model.ProblemContainer;
 import google.com.ortona.hashcode.final_2015.model.SolutionContainer;
+import google.com.ortona.hashcode.final_2015.model.Status;
 
+//for each baloon b:
+//	for each move m:
+//		compute covered cell with m
+//		for each other baloon b':
+//			compute distance with b'
+//	pick best move m based on covered cell and distance with other baloons
 public class ProblemSolver {
-  private static Logger LOG = LoggerFactory.getLogger(ProblemSolver.class);
+	private static Logger LOG = LoggerFactory.getLogger(ProblemSolver.class);
 
-  public SolutionContainer solve(ProblemContainer problem) {
-    return null;
-  }
+	public SolutionContainer solve(ProblemContainer problem) {
+		final Status s = problem.getStatus();
 
-  public static void main(String[] args) {
-    LOG.info("Hello World!");
-  }
+		// iterate for all turns
+		// for every turns, iterate over baloons B
+		// pick the best move for B
+		// move B
+
+	}
+
+	public static void main(String[] args) {
+		LOG.info("Hello World!");
+	}
 
 }
