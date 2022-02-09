c8d10a5f746d3897198085988d0e2bfdafa11708
carlo luchessa <c.luchessa@reply.it>
2022-02-09 22:52:55 +0100

    up.

--- src/main/java/google/com/ortona/hashcode/y_2022/pizza/logic/ProblemSolver.java
+++ src/main/java/google/com/ortona/hashcode/y_2022/pizza/logic/ProblemSolver.java
@@ -15,92 +15,98 @@ import google.com.ortona.hashcode.y_2022.pizza.model.ProblemContainer;
 import google.com.ortona.hashcode.y_2022.pizza.model.SolutionContainer;
 
 public class ProblemSolver {
-	private static Logger LOG = LoggerFactory.getLogger(ProblemSolver.class);
+    private static Logger LOG = LoggerFactory.getLogger(ProblemSolver.class);
 
-	private SolutionContainer solutionContainer = new SolutionContainer();
+    private SolutionContainer solutionContainer = new SolutionContainer();
 
-	Set<String> takenIngredients = new HashSet<>();
-
-	public SolutionContainer solve(ProblemContainer problem) {
-		boolean condition = true;
-
-		int bestScore = -1;
-		List<String> finalIngredients = null;
-
-		while (condition) {
-			Map<String, Integer> ingr2score = updateIngredientScore(problem.getClients(), takenIngredients);
-			int currentScore = (int) problem.getClients().stream().filter(c -> c.getTaken()).count();
-			LOG.debug("Current score: {} with {} ingredients", currentScore, takenIngredients.size());
-			if (currentScore > bestScore) {
-				bestScore = currentScore;
-				finalIngredients = new ArrayList<>();
-				finalIngredients.addAll(takenIngredients);
-			}
-			Client best = getBestClient(problem.getClients(), ingr2score);
-			takenIngredients.addAll(best.getLikes());
-			// compute current score
-
-		}
+    Set<String> takenIngredients = new HashSet<>();
 
 
-		SolutionContainer sC = new SolutionContainer();
-		return solutionContainer;
-	}
+    public SolutionContainer solve(ProblemContainer problem) {
+
+        boolean shouldStop = false;
+        int bestScore = -1;
+        List<String> finalIngredients = null;
+
+        while (true) {
+            Map<String, Integer> ingr2score = updateIngredientScore(problem.getClients(), takenIngredients);
+            int currentScore = (int) problem.getClients().stream().filter(c -> c.getTaken()).count();
+
+            LOG.debug("Current score: {} with {} ingredients", currentScore, takenIngredients.size());
+
+            if (currentScore > bestScore) {
+                bestScore = currentScore;
+                finalIngredients = new ArrayList<>();
+                finalIngredients.addAll(takenIngredients);
+            }
+
+            if (shouldStop) break; // EXIT
+
+            Client best = getBestClient(problem.getClients(), ingr2score);
+            int nIngr = takenIngredients.size();
+            takenIngredients.addAll(best.getLikes());
+            shouldStop = nIngr == takenIngredients.size();
+        }
 
 
-	private Client getBestClient(List<Client> allClients, Map<String, Integer> ingr2score) {
-		Client bestClient = null;
-		int bestScore = Integer.MIN_VALUE;
-		for (Client c : allClients) {
-			if (c.getTaken() != null) {
-				continue;
-			}
-			//compute score of the client and return the client with the best score
-			// skip already taken ingredients
-			int score = (int) c.getLikes().stream().filter(ingr -> !takenIngredients.contains(ingr)).count()
-					- (int) c.getDislikes().stream().filter(ingr -> !takenIngredients.contains(ingr)).count();
-			if (bestClient == null || score > bestScore) {
-				bestClient = c;
-				bestScore = score;
-			}
-
-		}
-		return bestClient;
-	}
+        SolutionContainer sC = new SolutionContainer();
+        return solutionContainer;
+    }
 
 
-	private Map<String, Integer> updateIngredientScore(List<Client> allClients, Set<String> takenIngredients) {
-		Map<String, Integer> ing2score = new HashMap<>();
-		for (Client c : allClients) {
-			if (c.getTaken() != null && !c.getTaken()) {
-				continue;
-			}
-			if (c.getDislikes().stream().anyMatch(ing -> takenIngredients.contains(ing))) {
-				c.setTaken(false);
-			} else {
-				if (c.getLikes().stream().allMatch(ing -> takenIngredients.contains(ing))) {
-					c.setTaken(true);
-				}
-			}
-			for (String oneIng : c.getDislikes()) {
-				if (!takenIngredients.contains(oneIng)) {
-					ing2score.put(oneIng, ing2score.getOrDefault(oneIng, 0) - 1);
-				}
-			}
+    private Client getBestClient(List<Client> allClients, Map<String, Integer> ingr2score) {
+        Client bestClient = null;
+        int bestScore = Integer.MIN_VALUE;
+        for (Client c : allClients) {
+            if (c.getTaken() != null) {
+                continue;
+            }
+            //compute score of the client and return the client with the best score
+            // skip already taken ingredients
+            int score = (int) c.getLikes().stream().filter(ingr -> !takenIngredients.contains(ingr)).count()
+                    - (int) c.getDislikes().stream().filter(ingr -> !takenIngredients.contains(ingr)).count();
+            if (bestClient == null || score > bestScore) {
+                bestClient = c;
+                bestScore = score;
+            }
 
-			if (c.getTaken() == null) {
-				for (String oneIng : c.getLikes()) {
-					if (!takenIngredients.contains(oneIng)) {
-						ing2score.put(oneIng, ing2score.getOrDefault(oneIng, 0) + 1);
-					}
-				}
-			}
-		}
-		return ing2score;
-	}
+        }
+        return bestClient;
+    }
 
-	public static void main(String[] args) {
-		LOG.info("Hello World!");
-	}
+
+    private Map<String, Integer> updateIngredientScore(List<Client> allClients, Set<String> takenIngredients) {
+        Map<String, Integer> ing2score = new HashMap<>();
+        for (Client c : allClients) {
+            if (c.getTaken() != null && !c.getTaken()) {
+                continue;
+            }
+            if (c.getDislikes().stream().anyMatch(ing -> takenIngredients.contains(ing))) {
+                c.setTaken(false);
+            } else {
+                if (c.getLikes().stream().allMatch(ing -> takenIngredients.contains(ing))) {
+                    c.setTaken(true);
+                }
+            }
+            for (String oneIng : c.getDislikes()) {
+                if (!takenIngredients.contains(oneIng)) {
+                    ing2score.put(oneIng, ing2score.getOrDefault(oneIng, 0) - 1);
+                }
+            }
+
+            if (c.getTaken() == null) {
+                for (String oneIng : c.getLikes()) {
+                    if (!takenIngredients.contains(oneIng)) {
+                        ing2score.put(oneIng, ing2score.getOrDefault(oneIng, 0) + 1);
+                    }
+                }
+            }
+        }
+        return ing2score;
+    }
+
+    public static void main(String[] args) {
+        LOG.info("Hello World!");
+    }
 
 }
