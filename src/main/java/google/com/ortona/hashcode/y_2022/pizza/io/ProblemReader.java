689fbf29bc516fb48aa472abd9d877cf74ff844c
Stefano Ortona <sortona@fb.com>
2022-02-09 22:03:01 +0000

    Final reader

--- src/main/java/google/com/ortona/hashcode/y_2022/pizza/io/ProblemReader.java
+++ src/main/java/google/com/ortona/hashcode/y_2022/pizza/io/ProblemReader.java
@@ -1,9 +1,46 @@
 package google.com.ortona.hashcode.y_2022.pizza.io;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import google.com.ortona.hashcode.y_2022.pizza.model.Client;
 import google.com.ortona.hashcode.y_2022.pizza.model.ProblemContainer;
 
 public class ProblemReader {
-	public ProblemContainer readProblem(String fileLocation) {
+	String folder = "src/main/resources/google/com/ortona/hashcode/y_2022.pizza2/io/";
+	public ProblemContainer readProblem(String fileLocation) throws IOException {
+		List<Client> allClients = new ArrayList<>();
+		BufferedReader br = new BufferedReader(new FileReader(new File(folder+fileLocation)));
+		
+		int clients = Integer.valueOf(br.readLine()); 
+		for(int i = 0; i < clients; i++) {
+			String likesS = br.readLine();
+			String dislikesS = br.readLine();
+			Client c = new Client();
+			c.id = i;
+			c.setLikes(getIngredients(likesS));
+			c.setDislikes(getIngredients(dislikesS));
+			allClients.add(c);
+		}
+		br.close();
+		
+		ProblemContainer pC = new ProblemContainer();
+		pC.setClients(allClients);
 		return null;
+	}
+	
+	
+	private List<String> getIngredients(String s){
+		String []tokens = s.split(" ");
+		List<String> ingredients = new ArrayList<>();
+		for(int i = 1; i < tokens.length; i++) {
+			ingredients.add(tokens[i]);
+		}
+		return ingredients;
 	}
 }
--- src/test/java/google/com/ortona/hashcode/y_2022/pizza/ProblemSolverTest.java
+++ src/test/java/google/com/ortona/hashcode/y_2022/pizza/ProblemSolverTest.java
@@ -55,9 +55,9 @@ public class ProblemSolverTest {
 
 	@Test
 	public void testFirstInput() throws IOException {
-		//final ProblemContainer p = READER.readProblem("a_example");
+		final ProblemContainer p = READER.readProblem("a_an_example");
 		//final ProblemContainer p = READER.readProblem("b_little_bit_of_everything.in");
-		final ProblemContainer p = READER.readProblem("c_many_ingredients.in");
+//		final ProblemContainer p = READER.readProblem("c_many_ingredients.in");
 		//final ProblemContainer p = READER.readProblem("d_many_pizzas.in");
 		//final ProblemContainer p = READER.readProblem("e_many_teams.in");
 
