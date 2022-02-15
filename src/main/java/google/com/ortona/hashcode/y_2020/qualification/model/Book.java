3f83db4173c964ed93d71f75963d33eecaa3f597
carlo luchessa <c.luchessa@reply.it>
2022-02-15 20:49:45 +0100

    model.

---
+++ src/main/java/google/com/ortona/hashcode/y_2020/qualification/model/Book.java
@@ -0,0 +1,61 @@
+package google.com.ortona.hashcode.y_2020.qualification.model;
+
+import jnr.constants.platform.PRIO;
+
+import java.util.Objects;
+
+public class Book {
+
+    private int id;
+
+    private int score;
+
+    private Integer libraryId = null;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public Integer getLibraryId() {
+        return libraryId;
+    }
+
+    public void setLibraryId(Integer libraryId) {
+        this.libraryId = libraryId;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", score=" + score +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return id == book.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
---
+++ src/main/java/google/com/ortona/hashcode/y_2020/qualification/model/Library.java
@@ -0,0 +1,83 @@
+package google.com.ortona.hashcode.y_2020.qualification.model;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Library {
+
+    private int id;
+
+    private int books4days;
+
+    private int signup;
+
+    private List<Book> bookList;
+
+    private int signupDay;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getBooks4days() {
+        return books4days;
+    }
+
+    public void setBooks4days(int books4days) {
+        this.books4days = books4days;
+    }
+
+    public int getSignup() {
+        return signup;
+    }
+
+    public void setSignup(int signup) {
+        this.signup = signup;
+    }
+
+    public List<Book> getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(List<Book> bookList) {
+        this.bookList = bookList;
+    }
+
+    public int getSignupDay() {
+        return signupDay;
+    }
+
+    public void setSignupDay(int signupDay) {
+        this.signupDay = signupDay;
+    }
+
+    @Override
+    public String toString() {
+        return "Library{" +
+                "id=" + id +
+                ", books4days=" + books4days +
+                ", signup=" + signup +
+                ", bookList=" + bookList +
+                ", signupDay=" + signupDay +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Library library = (Library) o;
+        return id == library.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
