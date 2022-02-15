030d8bbd31d77f2ac2048b44f07e816360ff31e9
Giorgio27 <giopag27@gmail.com>
2022-02-15 21:44:16 +0100

    test and solutionContainer to string

--- src/test/java/google/com/ortona/hashcode/y_2020/qualification/ProblemSolverTest.java
+++ src/test/java/google/com/ortona/hashcode/y_2020/qualification/ProblemSolverTest.java
@@ -1,6 +1,7 @@
 package google.com.ortona.hashcode.y_2020.qualification;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -10,6 +11,8 @@ import org.slf4j.LoggerFactory;
 import google.com.ortona.hashcode.y_2020.qualification.io.ProblemReader;
 import google.com.ortona.hashcode.y_2020.qualification.io.ProblemWriter;
 import google.com.ortona.hashcode.y_2020.qualification.logic.ProblemSolver;
+import google.com.ortona.hashcode.y_2020.qualification.model.Book;
+import google.com.ortona.hashcode.y_2020.qualification.model.Library;
 import google.com.ortona.hashcode.y_2020.qualification.model.ProblemContainer;
 import google.com.ortona.hashcode.y_2020.qualification.model.SolutionContainer;
 /**
@@ -27,12 +30,41 @@ public class ProblemSolverTest {
 	public void firstTest() {
 		LOG.info("----------------------");
 		LOG.info("First test is starting");
+		final Book b1 = new Book();
+		b1.setId(0);
+		b1.setScore(1);
+		final Book b2 = new Book();
+		b2.setId(1);
+		b2.setScore(2);
+		final Book b3 = new Book();
+		b3.setId(2);
+		b3.setScore(3);
+		final Book b4 = new Book();
+		b4.setId(3);
+		b4.setScore(6);
+		final Book b5 = new Book();
+		b5.setId(4);
+		b5.setScore(5);
+		final Book b6 = new Book();
+		b6.setId(5);
+		b6.setScore(4);
+		final Library l1 = new Library();
+		l1.setBookList(Arrays.asList(b1,b2,b3,b4,b5));
+		l1.setBooks4days(2);
+		l1.setSignup(2);
+		final Library l2 = new Library();
+		l2.setBookList(Arrays.asList(b4,b3,b6,b1));
+		l2.setBooks4days(1);
+		l2.setSignup(3);
+		
 		ProblemContainer pC = new ProblemContainer();
+		pC.LIBRARY_LIST = Arrays.asList(l1,l2);
 		// create here a mock input for test
 
 		
 		ProblemSolver pS = new ProblemSolver();
 		SolutionContainer sC = pS.solve(pC);
+		Assert.assertNotNull(sC);
 		LOG.info("Final score is: {}, with solution: {}",sC.getScore(), sC.toString());
 
 	}
